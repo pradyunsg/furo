f0a7a4cb47de55c2a94c1da61a805c99a7baaebc
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2020-08-15 12:22:11 +0530

    Add the Sphinx stuff

--- src/mawek/__init__.py
+++ src/mawek/__init__.py
@@ -1,3 +1,12 @@
 """I still have to figure out what this would be."""
 
 __version__ = "0.1.0.dev0"
+
+from pathlib import Path
+
+theme_path = (Path(__file__).parent / "templates").resolve()
+
+
+def setup(app):
+    """Entry point for sphinx theming."""
+    app.add_html_theme("mawek", str(theme_path))
