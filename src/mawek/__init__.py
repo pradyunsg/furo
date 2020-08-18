832191343ddebc5a0cfe65223e6de3bf22b9400a
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2020-08-18 18:09:03 +0530

    Simplify setting mawek_navigation_tree

--- src/mawek/__init__.py
+++ src/mawek/__init__.py
@@ -7,19 +7,15 @@ from pathlib import Path
 from .navigation import get_navigation_tree
 
 
-def add_navigation_tree_to(context):
-    """Adds mawek's custom navigation tree HTML into the context."""
-    toctree = context["toctree"]
+def sphinx_html_page_context(app, pagename, templatename, context, doctree):
+    """Handler for Sphinx's `html-page-context` event."""
+
+    # Custom Navigation Tree (adds checkboxes and labels)
+    toctree = context.get("toctree", lambda **kwargs: "")
     toctree_html = toctree(
         collapse=False, titles_only=True, maxdepth=-1, includehidden=True
     )
-
     context["mawek_navigation_tree"] = get_navigation_tree(toctree_html)
-
-
-def sphinx_html_page_context(app, pagename, templatename, context, doctree):
-    """Handler for Sphinx's `html-page-context` event."""
-    add_navigation_tree_to(context)
 
 
 def setup(app):
