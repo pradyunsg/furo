4337dec9f054cd3ac2810fe819ed3bfa415a1b1a
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2020-08-30 15:38:36 +0530

    Selectively reuse venv

--- noxfile.py
+++ noxfile.py
@@ -13,7 +13,6 @@ import nox
 
 PACKAGE_NAME = "mawek"
 nox.options.sessions = ["lint", "test"]
-nox.options.reuse_existing_virtualenvs = True
 
 
 #
@@ -84,15 +83,15 @@ def docs_live(session):
             )
 
 
-@nox.session(python="3.8")
+@nox.session(python="3.8", reuse_venv=True)
 def docs(session):
-    _install_this_project_with_flit(session, extras=["doc"], editable=True)
+    _install_this_project_with_flit(session, extras=["doc"], editable=False)
 
     # Generate documentation into `build/docs`
     session.run("sphinx-build", "-b", "html", "-v", "docs/", "build/docs")
 
 
-@nox.session(python="3.8")
+@nox.session(python="3.8", reuse_venv=True)
 def lint(session):
     session.install("pre-commit")
 
