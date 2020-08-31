25ddb47146f30e23ba30e7106b8cee920e4c67dc
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2020-08-31 19:42:19 +0530

    Prepare main.js for optimization

--- src/mawek/assets/scripts/main.js
+++ src/mawek/assets/scripts/main.js
@@ -1,66 +1,66 @@
-var header = null;
-var tocScroll = null;
+(function (window, document) {
+  var header = null;
+  var tocScroll = null;
 
-function scrollHandler(positionY) {
-  if (positionY == 0) {
-    header.classList.remove("scrolled");
-  } else {
-    header.classList.add("scrolled");
-  }
-
-  if (tocScroll === null) {
-    return;
-  }
-
-  if (positionY == 0) {
-    tocScroll.scrollTo(0, 0);
-  } else if (
-    Math.ceil(window.innerHeight + positionY) >= document.body.offsetHeight
-  ) {
-    tocScroll.scrollTo(0, tocScroll.scrollHeight);
-  }
-}
-
-function setupScrollHandler() {
-  // Taken from https://developer.mozilla.org/en-US/docs/Web/API/Document/scroll_event
-  let last_known_scroll_position = 0;
-  let ticking = false;
-
-  window.addEventListener("scroll", function (e) {
-    last_known_scroll_position = window.scrollY;
-
-    if (!ticking) {
-      window.requestAnimationFrame(function () {
-        scrollHandler(last_known_scroll_position);
-        ticking = false;
-      });
-
-      ticking = true;
+  function scrollHandler(positionY) {
+    if (positionY == 0) {
+      header.classList.remove("scrolled");
+    } else {
+      header.classList.add("scrolled");
     }
-  });
-}
 
-function setup() {
-  setupScrollHandler();
+    if (tocScroll === null) {
+      return;
+    }
 
-  if (tocScroll === null) {
-    return;
+    if (positionY == 0) {
+      tocScroll.scrollTo(0, 0);
+    } else if (
+      Math.ceil(window.innerHeight + positionY) >= document.body.offsetHeight
+    ) {
+      tocScroll.scrollTo(0, tocScroll.scrollHeight);
+    }
   }
 
-  console.log(tocScroll);
+  function setupScrollHandler() {
+    // Taken from https://developer.mozilla.org/en-US/docs/Web/API/Document/scroll_event
+    var last_known_scroll_position = 0;
+    var ticking = false;
 
-  // Scrollspy -- highlight table on contents, based on scroll
-  let spy = new Gumshoe(".toc-tree a", {
-    reflow: true,
-    recursive: true,
-    navClass: "scroll-current",
+    window.addEventListener("scroll", function (e) {
+      last_known_scroll_position = window.scrollY;
+
+      if (!ticking) {
+        window.requestAnimationFrame(function () {
+          scrollHandler(last_known_scroll_position);
+          ticking = false;
+        });
+
+        ticking = true;
+      }
+    });
+  }
+
+  function setup() {
+    setupScrollHandler();
+
+    if (tocScroll === null) {
+      return;
+    }
+
+    // Scrollspy -- highlight table on contents, based on scroll
+    var spy = new Gumshoe(".toc-tree a", {
+      reflow: true,
+      recursive: true,
+      navClass: "scroll-current",
+    });
+  }
+
+  document.addEventListener("DOMContentLoaded", function main(params) {
+    document.body.parentNode.classList.remove("no-js");
+
+    header = document.querySelector("header");
+    tocScroll = document.querySelector(".toc-scroll");
+    setup();
   });
-}
-
-document.addEventListener("DOMContentLoaded", function main(params) {
-  document.body.parentNode.classList.remove("no-js");
-
-  header = document.querySelector("header");
-  tocScroll = document.querySelector(".toc-scroll");
-  setup();
-});
+})(window, document);
