09ba6d502469bd054ae5d4384a066a725cc58082
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2020-11-02 08:04:19 +0530

    Simplify main.js

--- src/furo/assets/scripts/main.js
+++ src/furo/assets/scripts/main.js
@@ -1,63 +1,80 @@
-(function (window, document) {
-  var header = null;
-  var tocScroll = null;
+////////////////////////////////////////////////////////////////////////////////
+// Scroll Handling
+////////////////////////////////////////////////////////////////////////////////
+var tocScroll = null;
+var header = null;
 
-  function scrollHandler(positionY) {
-    if (header.getBoundingClientRect().top == 0) {
-      header.classList.add("scrolled");
-    } else {
-      header.classList.remove("scrolled");
-    }
+function scrollHandlerForHeader() {
+  if (header.getBoundingClientRect().top == 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+}
 
-    if (tocScroll === null) {
-      return;
-    }
-
-    if (positionY == 0) {
-      tocScroll.scrollTo(0, 0);
-    }
+function scrollHandlerForTOC(positionY) {
+  if (tocScroll === null) {
+    return;
   }
 
-  function setupScrollHandler() {
-    // Taken from https://developer.mozilla.org/en-US/docs/Web/API/Document/scroll_event
-    var last_known_scroll_position = 0;
-    var ticking = false;
-
-    window.addEventListener("scroll", function (e) {
-      last_known_scroll_position = window.scrollY;
-
-      if (!ticking) {
-        window.requestAnimationFrame(function () {
-          scrollHandler(last_known_scroll_position);
-          ticking = false;
-        });
-
-        ticking = true;
-      }
-    });
-    window.scroll();
+  if (positionY == 0) {
+    tocScroll.scrollTo(0, 0);
   }
+}
 
-  function setup() {
-    setupScrollHandler();
+function scrollHandler(positionY) {
+  scrollHandlerForHeader();
+  scrollHandlerForTOC(positionY);
+}
 
-    if (tocScroll === null) {
-      return;
+////////////////////////////////////////////////////////////////////////////////
+// Setup
+////////////////////////////////////////////////////////////////////////////////
+function setupScrollHandler() {
+  // Taken from https://developer.mozilla.org/en-US/docs/Web/API/Document/scroll_event
+  var last_known_scroll_position = 0;
+  var ticking = false;
+
+  window.addEventListener("scroll", function (e) {
+    last_known_scroll_position = window.scrollY;
+
+    if (!ticking) {
+      window.requestAnimationFrame(function () {
+        scrollHandler(last_known_scroll_position);
+        ticking = false;
+      });
+
+      ticking = true;
     }
-
-    // Scrollspy -- highlight table on contents, based on scroll
-    var spy = new Gumshoe(".toc-tree a", {
-      reflow: true,
-      recursive: true,
-      navClass: "scroll-current",
-    });
-  }
-
-  document.addEventListener("DOMContentLoaded", function main(params) {
-    document.body.parentNode.classList.remove("no-js");
-
-    header = document.querySelector("header");
-    tocScroll = document.querySelector(".toc-scroll");
-    setup();
   });
-})(window, document);
+  window.scroll();
+}
+
+function setupScrollSpy() {
+  if (tocScroll === null) {
+    return;
+  }
+
+  // Scrollspy -- highlight table on contents, based on scroll
+  var spy = new Gumshoe(".toc-tree a", {
+    reflow: true,
+    recursive: true,
+    navClass: "scroll-current",
+  });
+}
+
+function setup() {
+  setupScrollHandler();
+  setupScrollSpy();
+}
+
+function main() {
+  document.body.parentNode.classList.remove("no-js");
+
+  header = document.querySelector("header");
+  tocScroll = document.querySelector(".toc-scroll");
+
+  setup();
+}
+
+document.addEventListener("DOMContentLoaded", main);
