b37c525f89a0caba0dcac2d924a3c5f880946488
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2021-02-01 17:47:24 +0000

    Stop `defer`ing JS tags

--- src/furo/__init__.py
+++ src/furo/__init__.py
@@ -126,16 +126,6 @@ def _html_page_context(app, pagename, templatename, context, doctree):
     if app.config.html_theme != "furo":
         return
 
-    # Add attributes to the scripts loaded with `js_tag`
-    sphinx_js_tag = context["js_tag"]
-
-    def furo_js_tag(js):
-        if isinstance(js, JavaScript):
-            js.attributes["defer"] = "defer"
-        return sphinx_js_tag(js)
-
-    context["js_tag"] = furo_js_tag
-
     # Basic constants
     context["furo_version"] = __version__
     context["furo_asset_hash"] = furo_asset_hash
