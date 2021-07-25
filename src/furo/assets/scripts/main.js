d0f880da9453d6a5c2363b9b5c24927ed0c2747f
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2021-07-25 18:07:13 +0100

    First iteration of dark mode toggles

--- src/furo/assets/scripts/main.js
+++ src/furo/assets/scripts/main.js
@@ -50,6 +50,45 @@ function scrollHandler(positionY) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+// Theme Toggle
+////////////////////////////////////////////////////////////////////////////////
+function setTheme(mode) {
+  if (mode !== "light" && mode !== "dark" && mode !== "auto") {
+    console.error(`Got invalid theme mode: ${mode}. Resetting to auto.`);
+    mode = "auto";
+  }
+
+  document.body.dataset.theme = mode;
+  localStorage.setItem("theme", mode);
+  console.log(`Changed to ${mode} mode.`);
+}
+
+function cycleThemeOnce() {
+  const currentTheme = localStorage.getItem("theme") || "auto";
+  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+  if (prefersDark) {
+    // Auto (dark) -> Light -> Dark
+    if (currentTheme === "auto") {
+      setTheme("light");
+    } else if (currentTheme == "light") {
+      setTheme("dark");
+    } else {
+      setTheme("auto");
+    }
+  } else {
+    // Auto (light) -> Dark -> Light
+    if (currentTheme === "auto") {
+      setTheme("dark");
+    } else if (currentTheme == "dark") {
+      setTheme("light");
+    } else {
+      setTheme("auto");
+    }
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
 // Setup
 ////////////////////////////////////////////////////////////////////////////////
 function setupScrollHandler() {
@@ -85,7 +124,19 @@ function setupScrollSpy() {
   });
 }
 
+function setupTheme() {
+  // Set the current theme to auto.
+  setTheme(localStorage.getItem("theme") || "auto");
+
+  // Attach event handlers for toggling themes
+  const buttons = document.getElementsByClassName("theme-toggle");
+  Array.from(buttons).forEach((btn) => {
+    btn.addEventListener("click", cycleThemeOnce);
+  });
+}
+
 function setup() {
+  setupTheme();
   setupScrollHandler();
   setupScrollSpy();
 }
