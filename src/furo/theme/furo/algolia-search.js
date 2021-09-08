b0634875d128eb12994e79c8440cc9c3f724d0f9
Samhita Alla <aallasamhita@gmail.com>
2021-09-08 12:06:27 +0530

    fix algolia
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>

---
+++ src/furo/theme/furo/algolia-search.js
@@ -0,0 +1,8 @@
+/*global docsearch*/
+
+docsearch({
+    inputSelector: '.sidebar-search',
+    apiKey: 'af11c512aa92422933b48828d28dbe5f',
+    indexName: 'flyte',
+    debug: false // Set debug to true if you want to inspect the dropdown
+  });
