a5968632006d99ee4c3f699cc8950807c4d14855
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2021-11-15 09:03:27 +0000

    Add autoprefixer to webpack build pipeline

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           { loader: "css-loader", options: { sourceMap: true } },
+          { loader: "postcss-loader", options: { sourceMap: true } },
           { loader: "sass-loader", options: { sourceMap: true } },
         ],
       },
