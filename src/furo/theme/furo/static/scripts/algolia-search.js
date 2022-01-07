d95025cbd7c3200ed5b32b85ad76fb1209f95d1a
Samhita Alla <aallasamhita@gmail.com>
2022-01-07 12:22:47 +0530

    replace input with container
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>

--- src/furo/theme/furo/static/scripts/algolia-search.js
+++ src/furo/theme/furo/static/scripts/algolia-search.js
@@ -1,7 +1,7 @@
 /*global docsearch*/
 
 docsearch({
-  container: '.sidebar-search',
+  container: '#docsearch',
   appId: 'WLG0MZB58Q',
   apiKey: '28bf9bfd4a77a7d6b3ab7e98c671e781',
   indexName: 'flyte',
