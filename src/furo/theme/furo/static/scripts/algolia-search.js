4ec9a48bb90f62e3a56d886137c5292a8d03b979
Samhita Alla <aallasamhita@gmail.com>
2022-01-07 12:31:14 +0530

    add div search box
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>

--- src/furo/theme/furo/static/scripts/algolia-search.js
+++ src/furo/theme/furo/static/scripts/algolia-search.js
@@ -1,7 +1,7 @@
 /*global docsearch*/
 
 docsearch({
-  container: '#docsearch',
+  container: '.sidebar-search',
   appId: 'WLG0MZB58Q',
   apiKey: '28bf9bfd4a77a7d6b3ab7e98c671e781',
   indexName: 'flyte',
