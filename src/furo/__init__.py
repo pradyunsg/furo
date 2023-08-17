9b94420776972613c3990a31467855ceceefcff7
Pradyun Gedam <pradyunsg@gmail.com>
2023-08-17 16:43:40 +0000

    Accomodate for Sphinx 7.2 behavior change
    Sphinx 7 is now making backwards incompatible changes in patch releases.

--- src/furo/__init__.py
+++ src/furo/__init__.py
@@ -181,7 +181,7 @@ def _html_page_context(
         )
 
     if "css_files" in context:
-        if "_static/styles/furo.css" not in context["css_files"]:
+        if "_static/styles/furo.css" not in [c.filename for c in context["css_files"]]:
             raise Exception(
                 "This documentation is not using `furo.css` as the stylesheet. "
                 "If you have set `html_style` in your conf.py file, remove it."
