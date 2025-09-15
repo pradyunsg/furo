d92b62fee4cedd5b2ac1d267353d96541c9e0fb0
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2025-09-15 09:31:27 +0100

    Switch the dark mode theme back to `native`
    a11y-dark has bad console text contrast.

--- src/furo/__init__.py
+++ src/furo/__init__.py
@@ -380,7 +380,7 @@ def setup(app: sphinx.application.Sphinx) -> Dict[str, Any]:
     app.require_sphinx("6.0")
 
     app.add_config_value(
-        "pygments_dark_style", default="a11y-dark", rebuild="env", types=[str]
+        "pygments_dark_style", default="native", rebuild="env", types=[str]
     )
 
     app.add_html_theme("furo", str(THEME_PATH))
